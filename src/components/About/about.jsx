819d867ebc64dbd4412519baec42222bdcf1ee92
Kehuan <kehuanwang123@gmail.com>
2023-01-19 17:20:43 +1030

    update about

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -5,16 +5,22 @@ const About = () => {
         <div className="a">
             <h1 className="a-title">ABOUT ME</h1>
             <div className="underline"></div>
-            <p className="a-sub">
-                It is a long established fact that a reader will be distracted by the
-                readable content.
-            </p>
-            <p className="a-desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
-            </p>
+            <div className="a-sub">
+                <span style={{ "fontStyle": "italic" }}>keywords:</span> MScIT, software development, data engineering, React, JS/TS, HTML, CSS, Node
+            </div>
+
+            <div className="a-desc">
+                <p>
+                    Kehuan graduated from the Information Technology graduate program of Carnegie Mellon University, the Top 3 Computer Science University in the U.S.
+                    She has hands-on experience with object-oriented programming; developing, testing, troubleshooting, debugging; and managing systems and networks.
+                    Kehuan is also proficient in database management, data processing, and big data engineering.
+                </p>
+                <p>
+                    Kehuan is currently practicing full-stack software development in React, Javascript/Typescript/HTML5/CSS3, MongoDB, and Node.js.
+                    She enjoys exploring and mastering new technology, and she excels at working both independently and in teams.
+                    Kehuan's goal is to contribute her love of programming to advance a company's vision while enhancing her technical skills.
+                </p>
+            </div>
         </div>
     );
 };
